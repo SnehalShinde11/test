e91c933cb5f061ab79346996291c993fa0b7b892
SnehalShinde11 <83006384+SnehalShinde11@users.noreply.github.com>
2023-01-25 12:26:54 +0530

    delete webserver.sh

--- webserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yum -y install httpd
-echo "<h1>THIS WEBPAGE HAS BEEN BUILD WITH THE HELP OF JENKINS SERVER</h1>" > /var/www/html/index.html
-systemctl start httpd
-systemctl status httpd
-sleep 10
